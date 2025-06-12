439ce895ffe8c660362ea9df8bf773e7aa85813d
Xiao Yang <leeyuky9802@gmail.com>
2025-06-11 20:00:47 -0500

    2.9

--- packages/react-reconciler/src/ReactFiberCompleteWork.ts
+++ packages/react-reconciler/src/ReactFiberCompleteWork.ts
@@ -1,3 +1,51 @@
+import {
+  appendInitialChild,
+  createInstance,
+  createTextInstance,
+  type Instance,
+} from "ReactFiberConfig";
 import type { FiberNode } from "./ReactFiber";
+import { HostComponent, HostRoot, HostText } from "./ReactWorkTags";
+import { Update } from "./ReactFiberFlags";
+import type { HostComponentFiber } from "./ReactFiber/HostComponentFiber";
 
-export function completeWork(wipFiber: FiberNode): void {}
+export function completeWork(wipFiber: FiberNode): void {
+  switch (wipFiber.tag) {
+    case HostRoot:
+      return;
+    case HostComponent:
+      if (wipFiber.stateNode) {
+        // TODO: compare props to update the DOM
+      } else {
+        wipFiber.stateNode = createInstance(
+          wipFiber.type,
+          wipFiber.memoizedProps!
+        );
+        appendAllChildren(wipFiber);
+      }
+      return;
+    case HostText:
+      if (wipFiber.stateNode) {
+        // compare if the text is changed
+        const oldText = wipFiber.memoizedProps!.content;
+        const newText = wipFiber.pendingProps.content;
+
+        if (oldText !== newText) {
+          wipFiber.flags |= Update;
+        }
+      } else {
+        wipFiber.stateNode = createTextInstance(
+          wipFiber.memoizedProps!.content
+        );
+      }
+  }
+}
+
+function appendAllChildren(wipFiber: HostComponentFiber) {
+  if (wipFiber.child!.stateNode) {
+    appendInitialChild(
+      wipFiber.stateNode!,
+      wipFiber.child!.stateNode! as Instance
+    );
+  }
+}
--- packages/react-reconciler/src/ReactFiberFlags.ts
+++ packages/react-reconciler/src/ReactFiberFlags.ts
@@ -2,3 +2,4 @@ export type Flags = number;
 
 export const NoFlags = 0b0000000;
 export const Placement = 0b0000001;
+export const Update = 0b0000010;
